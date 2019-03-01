47dc2accd6412465fb78f0feda129a95b36185f4
RackReaver <rackreaver@gmail.com>
2019-02-28 20:28:34 -0500

    Setup versioneer for testing.

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+import versioneer
+
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    README = f.read()
+
+setup(
+    name='testing-package',
+    version=versioneer.get_version(),
+    url='https://github.com/RackReaver/testing-package',
+    license='Apache License',
+    author='Matt Ferreira',
+    author_email='rackreaver@gmail.com',
+    description='This is stricktly for testing purposes.',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.7'
+    ],
+    include_package_data=True,
+    install_requirements=[]
+)
